9279032479b2cacf3ce07a043d0379048dc9b111
ceadapon <charles.adapon@elembio.com>
2024-12-26 10:12:32 -0800

    progress

--- workspace/deck.go
+++ workspace/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // create new type of 'deck'
 // which is a slice of strings
@@ -10,16 +14,32 @@ func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clovers"}
 	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
-	for _, suit := range cardSuits{
+	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value + " of " + suit)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
-} 
+}
 
 func (d deck) print() {
-	for _, card := range d {
-		fmt.Println(card)
+	for i, card := range d {
+		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handsize int) (deck, deck) {
+	//create two new slices
+	//pop out number of cards from deck[0:handsize+1]
+	return d[:handsize], d[handsize:]
+
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+
 }
